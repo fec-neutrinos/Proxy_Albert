c7333ca80e68ea13e64d0775a2c7a976ae96ab26
Albert Kim <albertkim0722@gmail.com>
2019-10-19 10:21:51 -0700

    connected proxy server and rendered mine and Otto's service

--- server.js
+++ server.js
@@ -6,26 +6,15 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 app.use('*', cors());
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/*', express.static(path.join(__dirname, 'public')));
 
 app.get('/:productName', (req, res) => {
   res.sendFile((path.join(__dirname + '/public/index.html')))
 })
 
-app.get('/api/:productName', (req,res) => {
-  axios.get(`http://localhost:3001/api/${req.params.productName}`)
-  .then(results => {
-    console.log(results.data[0]);
-    res.send(results.data[0])
-  })
-  .catch(err => {
-    console.log(err);
-  })
-})
+
 app.listen(port, () => {
   console.log(`Proxy server is running on http://localhost:${port}`);
 });
-
